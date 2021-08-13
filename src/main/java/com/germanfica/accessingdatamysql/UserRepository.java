8b3f6d8af33efe8cd566d7fe3a76db44d54ccaf6
German Fica <germafica@gmail.com>
2021-08-12 21:41:51 -0300

    Create the User repository

---
+++ src/main/java/com/germanfica/accessingdatamysql/UserRepository.java
@@ -0,0 +1,7 @@
+package com.germanfica.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    
+}
